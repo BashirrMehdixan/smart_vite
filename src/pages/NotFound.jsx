48a0b53ea20c088ac604620852abacdfb1124d69
Bashirr <beshirr096@gmail.com>
2024-04-01 12:49:35 +0400

    not found page changed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,19 +5,6 @@ const NotFound = () => {
                 <div className="flex items-center justify-center text-5xl font-semibold py-3">
                     Not Found
                 </div>
-                <div>
-                    <div className="container">Nehnirinmiy nehniriniyyyy dutdurut durtdurut </div>54641466462163145615156
-                    <div>
-                        <p>
-                            Salam mən Mərcan . Tanişdiğimiza şad oldum . Buradan səslənirəm sevimli mentor Bəşirə . Ay bala aldığın kompüterə qusum bu nədir axı?
-                            Bugün 23 nisannnğğğğğ Atamdannn armağannnn sev gi ilee kolkolaaaaa birleşsinnn tüm dünyaaaaaa bu şarkı tümmm çocuklarfaaaaağaaaaaaaaaaaaaa
-
-                            lalaalalalalalaaaaaa lalalalalalaaaaaa lalalalalaaaaaaaaa
-
-
-                        </p>
-                    </div>
-                </div>
             </div>
         </>
     )
